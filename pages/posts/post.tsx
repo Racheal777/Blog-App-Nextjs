42ae632f4d05ef504bbfafe764b559959199bc3c
RachealKuranchie <racheal.kuranchie@eggseed.com>
2023-02-23 15:51:06 +0000

    first-commit

---
+++ pages/posts/post.tsx
@@ -0,0 +1,12 @@
+
+import Link from "next/link"
+
+
+export default function Post(){
+    return (
+        <div>
+            <h1>My first next post</h1>
+            <Link href="/">Back Home</Link>
+        </div>
+    )
+}
