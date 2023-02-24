f1ff167cce049ac5a1210e14c152210551607bef
Racheal777 <kuranchieracheal@gmail.com>
2023-02-24 20:59:36 +0000

    added a blog

--- pages/posts/post.tsx
+++ pages/posts/post.tsx
@@ -1,12 +1,26 @@
 
 import Link from "next/link"
+import Head from "next/head"
+import Layout from "../../components/layout"
+import { Children } from "react"
 
 
 export default function Post(){
     return (
         <div>
+            <Layout home={''}>
+
+            <Head>
+                <title>First post</title>
+            </Head>
+
             <h1>My first next post</h1>
-            <Link href="/">Back Home</Link>
+
+            
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo architecto eaque id. Nam beatae excepturi rem at enim deserunt ullam assumenda nobis veniam voluptate ratione iste veritatis dolorem, facilis tempore!</p>
+
+            </Layout>
+            
         </div>
     )
 }
